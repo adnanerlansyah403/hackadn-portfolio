d3ab3147a571e6ed57556cfade767332dd51ef99
adnanerlansyah403 <adnanerlansyah403@gmail.com>
2022-11-13 20:36:14 +0700

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,17 @@ import '../styles/require.css'
 import '../styles/project.css'
 import '../styles/about.css'
 import '../styles/contact.css'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <Component {...pageProps} />
+  )
 }
 
 export default MyApp
